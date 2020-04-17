9cfc49aa541350eab101dbe3ecd8f8c30a0a7fde
j721 <juliadvu@gmail.com>
2020-04-17 10:42:17 -0500

    switch statement laid out in reducer function

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -7,9 +7,31 @@ const initialState ={
 }
 
 const reducer = (state =initialState, action)=>{
-    return(
-        <div></div>
-    )
+    switch(action.type){
+        case FETCH_DATA_START:
+            return{
+                ...state, error: '', isFetching: true};
+        case FETCH_DATA_SUCCESS:
+            return{
+                ...state, error: '', isFetching:false, smurf: action.payload};
+        case FETCH_DATA_FAILURE:
+            return{
+                ...state, error: action.payload, isFetching: false};
+        
+
+        case POST_DATA_START:
+            return{
+                ...state, isFetching: true};
+        case POST_DATA_SUCCESS:
+            return{
+                ...state, smurf: action.payload, isFetching: false};
+        case POST_DATA_FAILURE:
+            return{
+                ...state, isFetching: false, error: action.payload};
+
+        default: 
+            return state; 
+    }
 }
 
 export default reducer; 

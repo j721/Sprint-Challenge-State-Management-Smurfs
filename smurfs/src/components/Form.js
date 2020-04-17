3ee4396aab96af953f73733c019f58c8d2172d2e
j721 <juliadvu@gmail.com>
2020-04-17 11:28:42 -0500

    Changes to List component used connect and mapStateToProps

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,64 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Form = (props)=>{
-    const [addedSmurf, setAddedSmurf] = useState({
-        name: '',
-        age: '',
-        height: ''
-    })
+const Form = (props) => {
+  const [addedSmurf, setAddedSmurf] = useState({
+    name: "",
+    age: "",
+    height: "",
+  });
 
-    const handleSubmit = e =>{
-        e.preventDefault(); 
-        props.addSmurf(addedSmurf);
-        setAddedSmurf({
-            ...newSmurf,
-            [e.target.name]: e.target.value
-        });
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addSmurf(addedSmurf);
+    setAddedSmurf({
+      ...newSmurf,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const changeHandler = e=>{
-        setAddedSmurf({
-            ...addedSmurf,
-            [e.target.name]: e.target.value
-        })
-    }
-return(
+  const changeHandler = (e) => {
+    setAddedSmurf({
+      ...addedSmurf,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
     <div>
-        <form onSubmit ={handleSubmit}>
-            <div>
-                <input
-                    type="text"
-                    name ="name"
-                    placeholder="Smurf's name"
-                    onChange ={changeHandler}
-                    value ={addedSmurf.name}
-                />
-            </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Smurf's name"
+            onChange={changeHandler}
+            value={addedSmurf.name}
+          />
+        </div>
 
-            <div>
-                <input
-                    type ="text"
-                    name ="age"
-                    placeholder ="Smurf's age"
-                    onChange ={changeHandler}
-                    value ={addedSmurf.age}
-                />
-            </div>
+        <div>
+          <input
+            type="text"
+            name="age"
+            placeholder="Smurf's age"
+            onChange={changeHandler}
+            value={addedSmurf.age}
+          />
+        </div>
 
-            <div>
-                <input
-                    type ="text"
-                    name ="height"
-                    placeholder ="Smurf's height"
-                    onChange ={changeHandler}
-                    value ={addedSmurf.height}
-                />
-            </div>
-                <button className ="submit" type ="submit">Create your Smurf!</button>
-        </form>
+        <div>
+          <input
+            type="text"
+            name="height"
+            placeholder="Smurf's height"
+            onChange={changeHandler}
+            value={addedSmurf.height}
+          />
+        </div>
+        <button className="submit" type="submit">
+          Create your Smurf!
+        </button>
+      </form>
     </div>
-    )
+  );
+}; //end of Form component
 
-}//end of Form component
-
-export default Form; 
+export default Form;

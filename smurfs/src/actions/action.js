41373d00c94d8350e98bdfb171bfec50b9809b01
j721 <juliadvu@gmail.com>
2020-04-17 10:54:50 -0500

    working on action.js file for get and post axios request

---
+++ smurfs/src/actions/action.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const fetchSmurfs = () =>dispatch=>{
+    dispatch({type: FETCH_DATA_START});
+
+    axios.get ("http://localhost:3333/smurfs")
+    .then(res=>{
+        dispatch({type: FETCH_DATA_SUCCESS, payload: res.data})
+    })
+    .catch(err=>{
+        console.log("data failed to return", err.response);
+        dispatch({
+            type: FETCH_DATA_FAILURE,
+            payload: `Error: ${err.response.status} ${err.response.data}`
+        });
+    });
+}
+
+export const addSmurf = newSmurf =>dispatch=>{
+    dispatch({type: POST_DATA_START});
+
+    axios.post("http://localhost:3333/smurfs")
+    .then(res=>{
+        console.log("post data returned", res);
+        dispatch({type: POST_DATA_SUCCESS, payload: res.data});
+    })
+    .catch(err=>{
+        console.log('post data failed to return', err);
+        dispatch({type: POST_DATA_FAILURE, payload: err })
+    })
+}

be1660e8a5d28874cd07d3beb5c5b0f0ecbff5c9
j721 <juliadvu@gmail.com>
2020-04-17 11:49:35 -0500

    changed to axios.delete for stretch to remove Smurf

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -8,6 +8,8 @@ export const POST_DATA_START ="POST_DATA_START";
 export const POST_DATA_SUCCESS ="POST_DATA_SUCCESS";
 export const POST_DATA_FAILURE ="POST_DATA_FAILURE";
 
+export const DELETE_SMURF ="DELETE_SMURF";
+
 export const fetchSmurfs = () =>dispatch=>{
     dispatch({type: FETCH_DATA_START});
 
@@ -37,3 +39,24 @@ export const addSmurf = addedSmurf =>dispatch=>{
         dispatch({type: POST_DATA_FAILURE, payload: err })
     })
 };
+
+//attempting stretch
+
+
+// export const DELETE_SMURF_START ='DELETE_SMURF_START';
+
+
+export const DELETE_SMURF_SUCCESS ='DELETE_SMURF_SUCCESS';
+export const DELETE_SMURF_FAIL ='DELETE_SMURF_FAIL';
+
+export const removeSmurf = id =>{
+    return dispatch=>{
+        axios.delete(`http://localhost:3333/smurfs${id}`)
+        .then(res =>{
+            dispatch({type:DELETE_SMURF_SUCCESS, payload: res.data })
+        })
+        .catch(err=>{
+            dispatch({type:DELETE_SMURF_FAIL, payload: err.response })
+        });
+    };
+}

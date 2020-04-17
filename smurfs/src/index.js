d4cc1bff43fe5bfd0468134faadc808f8a63dde9
j721 <juliadvu@gmail.com>
2020-04-17 10:18:15 -0500

    answered questions

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
